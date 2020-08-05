12e5e324c64aa8be207f3bf56df0651c53995761
Michael Perez <mperez6473@gmail.com>
2020-08-05 15:43:35 -0500

    Look up table done

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,3 +1,5 @@
+import random
+import math
 # Your code here
 
 
@@ -9,18 +11,55 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+cache = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    modMe = 982451653
+    z = x + y
 
+    # v = math.pow(x, y)  # x ^ y
+    # v = math.factorial(v)  # v is v *v-1 * v-2 * v-3 ... until 1
+    # v //= z  # new number after is divided by x + y
+    # v %= modMe  # moduloed by that num
+    if (x, y) not in cache:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[x, y] = v
+    return cache[x, y]
+    print(cache)
+    # w = 1
+    # u = 1
+
+    # for i in range(y):
+    #     w *= x
+    # for i in range(1, w):
+    #     u *= i
+    #     v = u
+    # v //= z
+    # v %= modMe
+    # cache[(x, y)] = v
+    # print(f'~~~cache here: {cache}')
+
+    return v
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+
+
+# 5, 4 624454993
+# 5, 3 179017319
+#      179017319
+#      476581239
